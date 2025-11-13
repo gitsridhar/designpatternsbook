28ced3b057af46f5743348979fbe1e377c78ed20
Sridhar Venkat <svenkat@us.ibm.com>
2025-11-13 09:23:47 -0500

    Additioal Files
    Signed-off-by: Sridhar Venkat <svenkat@us.ibm.com>

--- factorymethod/rust/factory_method/src/main.rs
+++ factorymethod/rust/factory_method/src/main.rs
@@ -1,10 +1,14 @@
 use consumer::consumer::Consumer;
 use consumer::consumer::AppleConsumer;
+use consumer::consumer::BananaConsumer;
 
-pub mod consumer;
+pub(crate) mod consumer;
 
 fn main() {
     println!("Hello, world!");
     let ac = AppleConsumer{};
     println!("{}",ac.consume().to_string());
+
+    let bc = BananaConsumer{};
+    println!("{}",bc.consume().to_string());
 }
